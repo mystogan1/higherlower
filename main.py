594b218488a09c74474e779cab72ccfd084f67b1
SASI-LAPTOP-ET9\mesas <mesasi2034@gmail.com>
2022-04-06 22:34:15 +0530

    List of TODOs

---
+++ main.py
@@ -0,0 +1,8 @@
+# start the game.
+# .logo
+# print A vs(.logo) B to compare. Which one have greater follower.
+# input("your choice")
+# create a dict with all choices and its values.
+# create a function to take random inputs from dict and return A and B
+# create a function to compare values of A and B.
+

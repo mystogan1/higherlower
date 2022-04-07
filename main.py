0ae76df813fcf7881ba8fafc5aa632927ad03ad4
SASI-LAPTOP-ET9\mesas <mesasi2034@gmail.com>
2022-04-07 12:27:58 +0530

    TODOs copied

---
+++ main.py
@@ -0,0 +1,13 @@
+# Display .logo
+
+# Generate a random account from gameData.
+
+# Format the account data in printable format.
+
+# Ask user to make a guess.
+
+# Check if user is correct
+# # Get follower count from gameData
+# # Use if statement to check if user is correct
+
+# Give user feedback on their guess.
